6a96b6fcd94ce9a25ecddcc4048f6cf438aa414b
Steven1400999 <quezadasteven570@gmail.com>
2024-12-01 00:13:44 -0600

    timeout

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 80000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: false,
